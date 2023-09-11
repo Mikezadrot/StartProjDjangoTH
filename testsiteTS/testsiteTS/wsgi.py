f5f99f9d25ab1cecea98754027a8b2f61c877903
mykhail <misha.nivievskiyy@gmail.com>
2023-09-11 12:31:31 +0300

    From M push to master(only Django startprogect)

---
+++ testsiteTS/testsiteTS/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testsiteTS project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testsiteTS.settings')
+
+application = get_wsgi_application()
