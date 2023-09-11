573c6c70f6dae8f4f1bc47bc438ed253cd891301
mykhail <misha.nivievskiyy@gmail.com>
2023-09-11 22:36:38 +0300

    Nextstep of murder

--- testsiteTS/testsiteTS/settings.py
+++ testsiteTS/testsiteTS/settings.py
@@ -73,10 +73,24 @@ WSGI_APPLICATION = 'testsiteTS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':  'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'yNrX2Dfx8L4OhDqWamqD',
+#         'HOST': 'containers-us-west-51.railway.app',
+#         'PORT': '7716'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'main_tested',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+
     }
 }
 
